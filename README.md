b0f1792333bfcba07d6513a93ec22daacb93a30d
Almudena Manzanal <amanzanalgo@alumni.unav.es>
2022-01-21 10:43:17 +0100

    Exportando csv

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 En este fichero se explica el contenido basico de nuestro repositorio.
 
 Guardamos el .md y le damos a commit, vemos los cambios y le damos a push par que se guarde.
+Se pueden poner carpetas, pdfs.. Todo se guarda ahí y se puede usar desde cualquier dispositivo.
+
+## Contenido
+
+### Exportar CSV
+Hemos exportado un CSV en el script *script_exportar.r*
--- script_exportar.R
+++ script_exportar.R
@@ -1,3 +1,29 @@
 library(MLDataR)
 tyr <- MLDataR::thyroid_disease
 write.csv(tyr, file = "dataset_tiroides.csv")
+
+sessionInfo() #poner al final de nuestros trabajos.
+
+#order
+
+head(tyr)
+
+sort(tyr$patient_age)
+
+tyr2 <- tyr[order(tyr$presc_anthyroid_meds),] #con sort solo ordena el vector pero 
+#con esto se ordenan todas las variables
+head(tyr2)
+
+#quitar todos los missing de un dataset
+tyr_completo <- tyr[complete.cases(tyr),]
+
+#aggregate -> agrupar variables
+
+#para ver edad media por geneero y dependiendo de si tienen tiroides o no 
+
+#aggregate(valores, por qué queremos agrupar, estadístico a usar)
+edad_tyr <- aggregate(tyr_completo$patient_age, list(tyr_completo$ThryroidClass, tyr_completo$patient_gender), mean)
+
+#para cambiar los nombres de las columnas de edad_tyr usamos names y concatenamos los nombres
+names(edad_tyr) <- c("Tiroides", "Genero", "Media")
+edad_tyr
